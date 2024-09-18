1cee0adbd38462d6a667bcd14d9d65a37758a9b6
Djohan93 <djohan93@hotmail.com>
2024-09-18 09:43:59 +0200

    Put the OOP exercises in the wrong folder, pushing again.

---
+++ ConsoleApp/klasser och objekt/6/Program.cs
@@ -0,0 +1,48 @@
+﻿Person per = new Person() {firstName = "Per", lastName = "Johansson" };
+Person annika = new Person() { firstName = "Annika", lastName = "Johansson" };
+Person myself = new Person() { firstName = "Daniel", lastName = "Johansson", father = per, mother = annika };
+
+
+
+
+Console.WriteLine(myself.GetSelfAndParents());
+
+
+
+
+
+
+
+
+class Person
+{
+    public string firstName = "Default";
+    public string lastName = "Defaultsson";
+    public Person mother;
+    public Person father;
+
+
+
+
+    public string GetFullName()
+    {
+        return $"{firstName} {lastName}";
+    }
+    public string GetFullName(string title)
+    {
+        return $"{title} {this.firstName} {this.lastName}";
+    }
+
+    public string GetFullNameReversed()
+    {
+        char[] chars = GetFullName().ToCharArray();
+        Array.Reverse(chars);
+        return new string(chars);
+    }
+
+    public string GetSelfAndParents()
+    {
+
+        return $"{GetFullName()} Mamma: {mother.GetFullName()}  Pappa: {father.GetFullName()}" ;
+    }
+}
---
+++ ConsoleApp/klasser och objekt/8/Program.cs
@@ -0,0 +1,83 @@
+﻿Person per = new Person() { firstName = "Per", lastName = "Johansson" };
+Person annika = new Person() { firstName = "Annika", lastName = "Johansson" };
+Person myself = new Person() { firstName = "Daniel", lastName = "Johansson", father = per, mother = annika };
+
+
+
+
+Console.WriteLine(myself.GetSelfAndParents());
+Console.WriteLine(per.GetSelfAndParents());
+Console.WriteLine(annika.GetSelfAndParents());
+myself.SetLength(1.90);
+Console.Write(myself.GetLength());
+
+
+
+
+
+
+
+class Person
+{
+    public string firstName = "Default";
+    public string lastName = "Defaultsson";
+    public Person mother;
+    public Person father;
+    private double length = 0;
+
+
+
+
+    public string GetFullName()
+    {
+        return $"{firstName} {lastName}";
+    }
+    public string GetFullName(string title)
+    {
+        return $"{title} {this.firstName} {this.lastName}";
+    }
+
+    public string GetFullNameReversed()
+    {
+        char[] chars = GetFullName().ToCharArray();
+        Array.Reverse(chars);
+        return new string(chars);
+    }
+
+    public string GetSelfAndParents()
+    {
+        string mothersName;
+        string fathersName;
+
+
+        if (this.father == null)
+        {
+            fathersName = "okänd";
+        }
+        else
+        {
+            fathersName = $"{father.firstName} {father.lastName}";
+        }
+
+        if (this.mother == null)
+        {
+            mothersName = "okänd";
+        }
+        else
+        {
+            mothersName = $"{mother.firstName} {mother.lastName}";
+
+        }
+        return $"{GetFullName()} Mamma: {mothersName}  Pappa: {fathersName}";
+    }
+
+    public void SetLength(double length)
+    {
+        this.length = length;
+    }
+
+    public double GetLength()
+    {
+        return this.length;
+    }
+}
