a60ea98b40c460c8301468a8e41aa13c5d35e47d
Djohan93 <djohan93@hotmail.com>
2024-10-17 19:55:35 +0200

    fixed small stuff

--- WPF/DialogBoxes/1/obj/Debug/net8.0-windows/1&2.AssemblyInfo.cs
+++ WPF/DialogBoxes/1/obj/Debug/net8.0-windows/1&2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("1&2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27dbd0c2822387153548041ae17bbb8e3f89380a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ea78d3f54d219a4b464c7d42fb37c0ad5c56ae1")]
 [assembly: System.Reflection.AssemblyProductAttribute("1&2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("1&2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
