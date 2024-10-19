79ae9ddbc5643e99aff8b26e40e843fd1f790f92
Djohan93 <djohan93@hotmail.com>
2024-10-19 19:41:54 +0200

    Removed some extra code.

--- WPF/DialogBoxes/5/MainWindow.xaml.cs
+++ WPF/DialogBoxes/5/MainWindow.xaml.cs
@@ -231,27 +231,5 @@ namespace _5
             return File.ReadAllBytes(dialog.FileName);
         }
 
-
-        public string StringToBinary(string data)
-        {
-            StringBuilder sb = new StringBuilder();
-            if(data != null) { 
-            foreach (char c in data.ToCharArray())
-                sb.Append(Convert.ToString(c, 2).PadLeft(8, '0'));
-            }
-            return sb.ToString();
-        }
-
-        public static string BinaryToString(string data)
-        {
-            List<Byte> byteList = new List<Byte>();
-
-            for (int i = 0; i < data.Length; i += 8)
-            {
-                byteList.Add(Convert.ToByte(data.Substring(i, 8), 2));
-            }
-            return Encoding.ASCII.GetString(byteList.ToArray());
-        }
-
     }
 }
