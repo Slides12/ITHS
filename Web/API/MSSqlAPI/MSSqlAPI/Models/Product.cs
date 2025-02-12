4dd0360d95bbf1eb6cb95e038ed2a2cccb72be29
Daniel Johansson <djohan93@hotmail.com>
2025-02-12 18:04:27 +0100

    Added a MS SQL API

---
+++ Web/API/MSSqlAPI/MSSqlAPI/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace MSSqlAPI.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+    public decimal Price { get; set; }
+    public string? ProductCategory { get; set; }
+    public bool Status { get; set; }
+}
---
+++ Web/API/MSSqlAPI/MSSqlAPI/SqlServerDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using MSSqlAPI.Models;
+
+namespace MSSqlAPI;
+
+public class SqlServerDBContext : DbContext
+{
+
+    public SqlServerDBContext(DbContextOptions<SqlServerDBContext> options) : base(options)
+    {
+    }
+
+    public DbSet<MSSqlAPI.Models.Product> Product { get; set; } = default!;
+
+}
