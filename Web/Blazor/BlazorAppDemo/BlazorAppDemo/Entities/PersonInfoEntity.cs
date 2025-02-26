a3d7e01478d8eabb17d9340feaf9d7be161b7953
Daniel Johansson <djohan93@hotmail.com>
2025-02-26 14:49:00 +0100

    Fixed DataAnnotations

--- Web/Blazor/BlazorAppDemo/BlazorAppDemo/Entities/PersonInfoEntity.cs
+++ Web/Blazor/BlazorAppDemo/BlazorAppDemo/Entities/PersonInfoEntity.cs
@@ -5,8 +5,12 @@ namespace BlazorAppDemo.Entities
     public class PersonInfoEntity
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Namn är obligatoriskt.")]
         public required string Name { get; set; }
+        [Required(ErrorMessage = "Email är ett måste.")]
+        [EmailAddress(ErrorMessage = "Ogiltig email-adress.")]
         public required string Email { get; set; }
+        [Range(1, 100, ErrorMessage = "Ålder måste ligga mellan 1-100.")]
         public int Age { get; set; }
     }
 }
