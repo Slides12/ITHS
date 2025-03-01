3e8d60633c48b51ccb22327cdca6fd0239d1a2b9
Daniel Johansson <djohan93@hotmail.com>
2025-03-01 15:46:32 +0100

    Added API with JWT Auth, roles, jwt token, refresh tokens

---
+++ Web/API/JwtAuthDotnet9/JwtAuthDotnet9/DTO/RefreshTokenRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace JwtAuthDotnet9.DTO
+{
+    public class RefreshTokenRequestDto
+    {
+        public Guid UserId { get; set; }
+        public required string RefreshToken { get; set; }
+    }
+}
---
+++ Web/API/JwtAuthDotnet9/JwtAuthDotnet9/Services/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using JwtAuthDotnet9.DTO;
+using JwtAuthDotnet9.Entities;
+
+namespace JwtAuthDotnet9.Services
+{
+    public interface IAuthService
+    {
+        Task<User?> RegisterAsync(UserDto request);
+        Task<TokenResponseDto?> LoginAsync(UserDto request);
+        Task<TokenResponseDto> RefreshTokensAsync(RefreshTokenRequestDto request);
+    }
+}
