d0bc0523794e264f7f35cec57d69b177ac6c70cb
Daniel Johansson <djohan93@hotmail.com>
2025-11-10 14:44:14 +0100

    Övning 2 – Observer Pattern (Stock Market)

---
+++ Design Patterns/Observer Pattern/Observer Pattern/Classes/Stock.cs
@@ -0,0 +1,43 @@
+﻿namespace Observer_Pattern.Classes
+{
+    public class Stock
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+        public int oldPrice;
+
+        private List<Investors> investors = new List<Investors>();
+
+        public Stock(string name, int price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public void ChangePrice(int newPrice)
+        {
+            oldPrice = Price;
+            Price = newPrice;
+            Notify();
+        }
+
+        public void Attach(Investors investor)
+        {
+            investors.Add(investor);
+        }
+
+        public void Detach(Investors investor)
+        {
+            investors.Remove(investor);
+        }
+
+        public void Notify()
+        {
+            foreach (var investor in investors)
+            {
+                investor.Update(this);
+            }
+        }
+    }
+}
---
+++ Design Patterns/Observer Pattern/Observer Pattern/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+using Observer_Pattern.Classes;
+
+var investor1 = new Investors("Daniel");
+var investor2 = new Investors("Johan");
+var investor3 = new Investors("Sven");
+
+
+var stock1 = new Stock("Investor AB", 200);
+var stock2 = new Stock("Swedensia AB", 700);
+var stock3 = new Stock("Iths AB", 500);
+
+
+stock1.Attach(investor1);
+stock2.Attach(investor2);
+stock3.Attach(investor3);
+
+
+stock1.ChangePrice(1);
+stock2.ChangePrice(1);
+stock3.ChangePrice(1);
