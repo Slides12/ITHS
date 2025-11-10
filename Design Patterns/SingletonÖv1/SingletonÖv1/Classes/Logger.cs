c9e689fb2d96d41b94448e5bf768463bfb5f1d46
Daniel Johansson <djohan93@hotmail.com>
2025-11-10 14:58:02 +0100

    Övning 1 – Identifiera problem som Singleton löser

---
+++ Design Patterns/SingletonÖv1/SingletonÖv1/Classes/Logger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonÖv1.Classes
+{
+    public sealed class Logger
+    {
+        private static Logger _logger;
+
+        private List<string> _logs = new List<string>();
+
+        private Logger() { }
+
+        public static Logger GetInstance()
+        {
+            if (_logger == null)
+            {
+                _logger = new Logger();
+            }
+            return _logger;
+        }
+
+
+        public void Log(string message)
+        {
+            _logs.Add(message);
+
+            foreach (var item in _logs)
+                Console.WriteLine(item);
+        }
+    }
+}
---
+++ Design Patterns/SingletonÖv1/SingletonÖv1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SingletonÖv1.Classes;
+
+Console.WriteLine("Hello, World!");
+
+
+var logger = Logger.GetInstance();
+logger.Log("yo");
+logger.Log("hej");
+
+var logger2 = Logger.GetInstance();
+logger2.Log("något annat");
