be5b57ed2ed71d7e1502e1bac20210966ea81caa
Daniel Johansson <djohan93@hotmail.com>
2025-12-01 11:08:44 +0100

    Ocelot management api demo

---
+++ Apim/DemoApiGateway/DemoApiGateway/Controllers/Hello1Controller.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hello1.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Hello1Controller : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello from api 1");
+        }
+    }
+}
---
+++ Apim/DemoApiGateway/Hello2.api/Controllers/Hello2Controller.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hello2.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Hello2Controller : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello from api 2");
+        }
+    }
+}
